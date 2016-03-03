d5fc1094fcbda475a10555b58aba8a7cae9a1d6c
Laurent Victorino <lvictorino@monkeymoon.net>
2016-03-03 12:42:26 +0100

    Comments and guidelines for a cleaner code.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))] // Be sure that the object has a rigidbody to avoid surprises.
+public class BallController : MonoBehaviour
+{
+
+    public float speed;
+    public GameController game_controller;
+
+    private Rigidbody2D _rb;
+    private Vector2 _dir;
+
+	void Start ()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        if ( _rb == null)
+        {
+            Debug.LogError("Unable to find a Rigidbody2D on " + gameObject.name);
+        }
+    }
+
+    //void Update()
+    //{
+    //    //_rb.velocity = _dir; // Why updating the velocity every frame with the same value?
+    //    // Why not updating velocity only when _dir is actually changed (in SetDir) instead?
+    //}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        // "Player" and "Border" collision should act the same...
+        //if (col.gameObject.tag == "Player")
+        //{
+        //    _dir = col.contacts[0].normal.normalized * speed;
+        //}
+        //else if (col.gameObject.tag == "Border")
+        //{
+        //    _dir = new Vector2(_dir.x, _dir.y * -1);
+        //}
+        if ( col.gameObject.tag == "Player" || col.gameObject.tag == "Border")
+        {
+            // Always use methods you've created on purpose instead of doing the same line of code twice.
+            //_dir = col.contacts[0].normal.normalized * speed;
+            SetDir(col.contacts[0].normal.normalized);
+        }
+        else if (col.gameObject.name == "Left")
+        {
+            // GameObject.Find("GameController").GetComponent<GameController>().addScorePlayerLeft();  // Avoid GameObject.Find() + GameComponent if you can.
+            // Use a public field instead
+            game_controller.addScorePlayerLeft();
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.name == "Right")
+        {
+            //GameObject.Find("GameController").GetComponent<GameController>().addScorePlayerRight();
+            game_controller.addScorePlayerRight();
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetDir(Vector2 dir) 
+    //    public void setDir(Vector2 dir) // Methods should use a Camel Case.
+    {
+        _dir = dir * speed; // As you apply a speed on it _dir is a velocity, not really a direction... the name should be _velocity :)
+        _rb.velocity = _dir;
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,9 +2,18 @@
 using UnityEngine.UI;
 using System.Collections;
 
+[RequireComponent(typeof(BallController))] // be sure that GetComponent<BallController> works.
+// Game Controller should be on an isolated game_object... it feels weird to have it on the ball, especially when the ball has to communicate with GameController.
 public class GameController : MonoBehaviour
 {
-    public GameObject Ball;
+    //    public GameObject Ball; // Use lower case on fields
+    public GameObject ball;
+
+    public Text time_text;
+    public Text left_score_text;
+    public Text right_score_text;
+
+    private BallController _ball_controller;
 
     private int _playerRightScore;
     private int _playerLeftScore;
@@ -16,21 +25,49 @@ public class GameController : MonoBehaviour
     {
         _time = Time.time;
         _dir = new Vector2(1.0f, 0.0f);
+
+        _ball_controller = GetComponent<BallController>();
+        if (_ball_controller == null)
+        {
+            Debug.LogError("Unable to find BallController on " + gameObject.name);
+        }
+        if (time_text == null)
+        {
+            Debug.LogError("time_text field should not be empty.");
+        }
+        if (left_score_text == null)
+        {
+            Debug.LogWarning("left_score_text field should not be empty.");
+        }
+        if (right_score_text == null)
+        {
+            Debug.LogWarning("right_score_text field should not be empty.");
+        }
     }
-	
-	void Update ()
+
+    void Update ()
     {
 	    if (_start == true)
         {
-            GameObject.Find("Time").GetComponent<Text>().enabled = true;
+            // GameObject.Find costs a lot... avoid that in Update()
+            // Use a field instead
+            // GameObject.Find("Time").GetComponent<Text>().enabled = true;
+            time_text.enabled = true;
             if (Time.time - _time > 3)
             {
                 _start = false;
-                GameObject.Find("Time").GetComponent<Text>().enabled = false;
-                GameObject go = Instantiate(Ball, Ball.transform.position, Quaternion.identity) as GameObject;
-                go.GetComponent<BallController>().setDir(_dir);
+                //GameObject.Find("Time").GetComponent<Text>().enabled = false;
+                time_text.enabled = false;
+                GameObject go = Instantiate(ball, ball.transform.position, Quaternion.identity) as GameObject;
+                // Avoid GetComponent at every frame if possible
+                // go.GetComponent<BallController>().setDir(_dir);
+                if (_ball_controller != null)
+                {
+                    _ball_controller.SetDir(_dir);
+                }
             }
-            GameObject.Find("Time").GetComponent<Text>().text = (3 - ((int)Time.time - (int)_time)).ToString();
+            //GameObject.Find("Time").GetComponent<Text>().text = (3 - ((int)Time.time - (int)_time)).ToString();
+            time_text.text = (3 - ((int)Time.time - (int)_time)).ToString();
         }
         
 	}
@@ -38,7 +75,11 @@ public class GameController : MonoBehaviour
     public void addScorePlayerRight()
     {
         _playerRightScore += 1;
-        GameObject.Find("PlayerRightScore").GetComponent<Text>().text = _playerRightScore.ToString();
+//        GameObject.Find("PlayerRightScore").GetComponent<Text>().text = _playerRightScore.ToString();
+        if (right_score_text != null)
+        {
+            right_score_text.text = _playerRightScore.ToString();
+        }
         _start = true;
         _time = Time.time - 2.0f;
         _dir = new Vector2(1.0f, 0f);
@@ -47,7 +88,11 @@ public class GameController : MonoBehaviour
     public void addScorePlayerLeft()
     {
         _playerLeftScore += 1;
-        GameObject.Find("PlayerLeftScore").GetComponent<Text>().text = _playerLeftScore.ToString();
+        //GameObject.Find("PlayerLeftScore").GetComponent<Text>().text = _playerLeftScore.ToString();
+        if ( left_score_text != null )
+        {
+            left_score_text.text = _playerLeftScore.ToString();
+        }
         _start = true;
         _time = Time.time - 2.0f;
         _dir = new Vector2(-1.0f, 0f);
